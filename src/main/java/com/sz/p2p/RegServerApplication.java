65c8306e57ecaf3e6e179cbb672f81d99e00a9bb
gitcat7 <870375868@qq.com>
2019-09-17 16:53:50 +0800

    last update

--- src/main/java/com/sz/p2p/RegServerApplication.java
+++ src/main/java/com/sz/p2p/RegServerApplication.java
@@ -1,13 +1,11 @@
 package com.sz.p2p;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-@MapperScan("com.sz.p2p.dao")
 public class RegServerApplication {
 
 	public static void main(String[] args) {
